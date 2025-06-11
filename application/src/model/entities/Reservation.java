f5bedabdf33543e35ccae6503271c2a2b58db6f4
Renata <guiimaraesreenata@gmail.com>
2025-06-11 12:40:28 -0300

    Good solution

--- application/src/model/entities/Reservation.java
+++ application/src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -12,10 +14,14 @@ public class Reservation {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer roomNumber, Date checkOut, Date checkIn) {
+    public Reservation(Integer roomNumber, Date checkOut, Date checkIn) throws DomainException {
+        if (!checkOut.after(checkIn)){
+            throw new DomainException("Error in reservation: Check-out must be after check-in date");
+        }
+
         this.roomNumber = roomNumber;
-        this.checkOut = checkOut;
         this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public Integer getRoomNumber() {
@@ -26,12 +32,12 @@ public class Reservation {
         this.roomNumber = roomNumber;
     }
 
-    public Date getCheckOut() {
-        return checkOut;
-    }
-
     public Date getCheckIn() {
         return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
     }
 
     public long duration() {
@@ -39,19 +45,16 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Error in reservation: Reservation dates for update must be future dates!";
+            throw new DomainException("Error in reservation: Reservation dates for update must be future dates!");
         }
-        if (!checkOut.after(checkIn)){
-            return "Error in reservation: Check-out must be after check-in date";
-        }
+
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
 
     @Override
